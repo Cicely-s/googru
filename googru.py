60905445272fcda6406efb3941a12b3d298d6feb
Cicely-s <Cicely-s@users.noreply.github.com>
2021-05-12 16:45:53 +0900

    ぐぐるやつ。

--- googru.py
+++ googru.py
@@ -5,20 +5,20 @@ import webbrowser
 sg.theme('SystemDefault')
 
 layout = [[sg.InputText(key='-IN-'),
-           sg.Button('Search', key='-SEARCH-'),
-           sg.Button('Clear', key='-CLEAR-')],]
+        sg.Button('Search', key='-SEARCH-', bind_return_key=True),
+        sg.Button('Clear', key='-CLEAR-')],]
 
-window = sg.Window('ググるやつ。', layout, grab_anywhere=True)
+window = sg.Window('ググるやつ。', layout, grab_anywhere=True, keep_on_top=True)
 
 while True:
     event, values = window.read()
     print(event, values)
-    if event == sg.WIN_CLOSED or event == 'Exit':
+    if event == sg.WIN_CLOSED:
         break
     if event == '-SEARCH-':
         webbrowser.open('https://www.google.co.jp/search?q=' +
                         values['-IN-'] +'&ie=utf-8&oe-utf-8&hl=ja')
-    if event == '-CLEAR-':
+    elif event == '-CLEAR-':
         window['-IN-'].update('')
 
 window.close()
